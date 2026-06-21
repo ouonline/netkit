1877acba01ebeecfdd3164b6bf396efc7dd04a71
Guoyu Ou <benogy@gmail.com>
2026-06-21 23:11:03 +0800

    remove epoll impl

--- include/netkit/nq_utils.h
+++ include/netkit/nq_utils.h
@@ -1,33 +1,15 @@
 #ifndef __NETKIT_NQ_UTILS_H__
 #define __NETKIT_NQ_UTILS_H__
 
-#ifdef NETKIT_ENABLE_IOURING
 #include "netkit/iouring/notification_queue_impl.h"
-#elif defined(NETKIT_ENABLE_EPOLL)
-#include "netkit/epoll/notification_queue_impl.h"
-#else
-#error "`NotificationQueueImpl` is not defined."
-#endif
 
 namespace netkit {
-
-#ifdef NETKIT_ENABLE_IOURING
 
 using netkit::iouring::NotificationQueueImpl;
 
 inline int InitNq(NotificationQueueImpl* nq, Logger* l) {
     return nq->Init(netkit::iouring::NotificationQueueOptions(), l);
 }
-
-#elif defined(NETKIT_ENABLE_EPOLL)
-
-using netkit::epoll::NotificationQueueImpl;
-
-inline int InitNq(NotificationQueueImpl* nq, Logger* l) {
-    return nq->Init(l);
-}
-
-#endif
 
 }
 
